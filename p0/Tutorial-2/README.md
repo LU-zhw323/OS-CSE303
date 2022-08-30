35d73fa1e1cddac329414193e0dd50f9a419df48
ZhenyuWu323 <zhw323@lehigh.edu>
2022-08-29 21:42:07 -0400

    Tutorial 3

---
+++ p0/Tutorial-2/README.md
@@ -0,0 +1,2 @@
+# 事项
+这个makefile是给单个文件在dokcer里运行用的， 修改TARGETS, 创建container，然后make
---
+++ p0/Tutorial-3/printer.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "printer.h"
+
+/// Print a newline-terminated message to the console
+///
+/// @param message The message to print
+void printSomething(std::string message) { std::cout << message << std::endl; }
