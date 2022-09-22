21e3d1e14e18c72fe909c1fcc32705aa9a0c61e6
ZhenyuWu323 <zhw323@lehigh.edu>
2022-09-21 23:14:10 -0400

    mycrypto.cc

--- p1/common/my_crypto.cc
+++ p1/common/my_crypto.cc
@@ -8,6 +8,7 @@
 
 #include "protocol.h"
 using namespace std;
+using std::begin, std::end;
 
 /// Run the AES symmetric encryption/decryption algorithm on a buffer of bytes.
 /// Note that this will do either encryption or decryption, depending on how the
@@ -32,12 +33,32 @@ vector<uint8_t> aes_crypt_msg(EVP_CIPHER_CTX *ctx, const unsigned char *start,
   // figure out the block size that AES is going to use
   
   int cipher_block_size = EVP_CIPHER_block_size(EVP_CIPHER_CTX_cipher(ctx));
+
+  //vector to return
+  vector<uint8_t> encry_msg;
   
   // Set up a buffer where AES puts crypted bits. 
   //The output buffer length should at least be EVP_CIPHER_block_size() byte longer then the input length
   unsigned char out_buf[LEN_PROFILE_FILE + cipher_block_size];
+  int out_len;
   
-
-
+  // crypt msg into out_buf
+  if (!EVP_CipherUpdate(ctx, out_buf, &out_len, start, count)) {
+    fprintf(stderr, "Error in EVP_CipherUpdate: %s\n",
+          ERR_error_string(ERR_get_error(), nullptr));
+    return {};
+  }
+  encry_msg.insert(end(encry_msg), out_buf, out_buf + out_len);
+  
+    
+  // crypt the last block
+  if (!EVP_CipherFinal_ex(ctx, out_buf, &out_len)) {
+  fprintf(stderr, "Error in EVP_CipherFinal_ex: %s\n",
+          ERR_error_string(ERR_get_error(), nullptr));
   return {};
+}
+  encry_msg.insert(end(encry_msg), out_buf, out_buf + out_len);
+
+
+  return encry_msg;
 }
